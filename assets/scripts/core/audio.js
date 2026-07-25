511d006f19b210be80a579e1bfc289cb766f8804
SSI <130287955+SSI-GH@users.noreply.github.com>
2026-07-25 22:31:50 +0500

    fix: apply speedhack to custom songs (TestCommit)

--- assets/scripts/core/audio.js
+++ assets/scripts/core/audio.js
@@ -269,6 +269,10 @@ class AudioManager {
     const source = ctx.createBufferSource();
     source.buffer = audioBuffer;
     source.loop = true;
+    const currentSpeed = window.SpeedHack || 1;
+    if (source && source.playbackRate) {
+    source.playbackRate.setValueAtTime(currentSpeed, ctx.currentTime);
+    }
     source.connect(gainNode);
     source.start(0, safeOffset);
     this._onlineSource = source;
